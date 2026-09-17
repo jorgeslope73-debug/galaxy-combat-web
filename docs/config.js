fde649e54f126e82df34cfac35adf5a344d2ebf2
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-18 00:00:23 +0200

    Add files via upload

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,13 @@
+/*
+  GALAXY COMBAT WEB - CONFIGURACION
+
+  Para GitHub Pages pon aqui la URL HTTPS de tu servidor de partida.
+  Ejemplo:
+    serverUrl: 'https://galaxy-combat-servidor.onrender.com'
+
+  Si se deja vacio, el juego intenta usar el mismo servidor que sirve la web.
+  Eso es util al probar todo localmente, pero GitHub Pages necesita un backend externo.
+*/
 window.GALAXY_CONFIG = {
-  serverUrl: 'https://galaxy-combat-web.onrender.com'
+  serverUrl: ''
 };
