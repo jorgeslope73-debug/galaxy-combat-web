3be254eac5f2ce9fcd1389b3d3e7a379689a99f8
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-19 20:20:56 +0200

    Delete docs directory

--- docs/pwa.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-(() => {
-  if('serviceWorker' in navigator){window.addEventListener('load',()=>navigator.serviceWorker.register('./sw.js').catch(err=>console.warn('[Galaxy Combat] Service worker no disponible.',err)));}
-})();
