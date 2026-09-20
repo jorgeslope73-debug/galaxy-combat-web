cdc9733a8be4396848304f0c432f9e4ec26d68d1
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-20 13:03:34 +0200

    Add files via upload

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V16.4.48';
+const VERSION = 'V16.4.49';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
