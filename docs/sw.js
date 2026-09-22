c932023017d832a110e1099c4527726a3f980d37
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-22 09:49:28 +0200

    V16.4.70: amplia un poco mas el campo de nombre

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V16.4.69';
+const VERSION = 'V16.4.70';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
