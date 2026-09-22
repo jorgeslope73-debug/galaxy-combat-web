80f5677027b4d63b8efb549c8ed348874ac5488a
jorgeslope73-debug <jorgeslope73@gmail.com>
2026-09-22 23:01:08 +0200

    V16.4.98: marca como necesario añadir a pantalla de inicio

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'V16.4.97';
+const VERSION = 'V16.4.98';
 const SHELL_CACHE = `galaxy-combat-shell-${VERSION}`;
 const ASSET_CACHE = `galaxy-combat-assets-${VERSION}`;
 const ACTIVE_CACHES = new Set([SHELL_CACHE, ASSET_CACHE]);
